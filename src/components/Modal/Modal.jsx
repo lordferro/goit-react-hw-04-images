03ea5c0d81205c93ed454255c25c4c33205a4522
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-04-03 17:08:49 +0200

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,33 @@
-import { Component } from 'react';
+import { Component, useEffect } from 'react';
 import { Overlay, ModalStyled } from './Modal.styled';
 import PropTypes from 'prop-types';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = props => {
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+  function handleKeyDown(e) {
     if (e.code === 'Escape') {
-      this.props.onClick(e);
+      props.onClick(e);
     }
-  };
-
-  render() {
-    const { imageUrl, onClick } = this.props;
-    return (
-      <Overlay onClick={onClick}>
-        <ModalStyled>
-          <img src={imageUrl} alt="" />
-        </ModalStyled>
-      </Overlay>
-    );
   }
-}
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  const { imageUrl, onClick } = props;
+  return (
+    <Overlay onClick={onClick}>
+      <ModalStyled>
+        <img src={imageUrl} alt="" />
+      </ModalStyled>
+    </Overlay>
+  );
+};
 
 Modal.propTypes = {
   onClick: PropTypes.func.isRequired,
   imageUrl: PropTypes.string.isRequired,
-}
+};
